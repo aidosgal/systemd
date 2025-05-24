17bad322ebb38ee3cd265387bf5882e860529f63
aidosgal <agalimzhan928@gmail.com>
2025-05-24 08:49:54 +0500

    feat(concurrency): implement the channel

--- go_tech/concurrency/channel/main.go
+++ go_tech/concurrency/channel/main.go
@@ -7,11 +7,6 @@ import (
 	"time"
 )
 
-var (
-	totalUpdates int
-	updateMutex  sync.Mutex
-)
-
 type Order struct {
 	ID     int
 	Status string
@@ -20,28 +15,26 @@ type Order struct {
 
 func main() {
 	wg := sync.WaitGroup{}
-	wg.Add(3)
+	wg.Add(2)
 
-	orders := generateOrders(20)
+	orderChan := make(chan *Order, 20)
 
-	//go func() {
-	//	defer wg.Done()
-	//	processOrders(orders)
-	//}()
+	go func() {
+		defer wg.Done()
+		for _, order := range generateOrders(20) {
+			orderChan <- order
+		}
 
-	for i := 0; i < 3; i++ {
-		go func() {
-			defer wg.Done()
-			for _, order := range orders {
-				updateOrderStatus(order)
-			}
-		}()
-	}
+		close(orderChan)
+
+		fmt.Println("Done")
+	}()
+
+	go processOrders(orderChan, &wg)
 
 	// reportOrderStatus(orders)
 	wg.Wait()
 	fmt.Println("All operations compeleted.")
-	fmt.Println(totalUpdates)
 }
 
 func generateOrders(count int) []*Order {
@@ -56,37 +49,15 @@ func generateOrders(count int) []*Order {
 	return orders
 }
 
-func processOrders(orders []*Order) {
-	for _, order := range orders {
+func processOrders(orderChan <-chan *Order, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for order := range orderChan {
 		time.Sleep(
 			time.Duration(rand.Intn(500)) *
 				time.Millisecond,
 		)
 		fmt.Printf("Processing order %d\n", order.ID)
 	}
-}
-
-func updateOrderStatus(order *Order) {
-	order.mu.Lock()
-	time.Sleep(
-		time.Duration(rand.Intn(300)) *
-			time.Millisecond,
-	)
-	status := []string{
-		"Processing", "Shipped", "Delivered",
-	}[rand.Intn(3)]
-	order.Status = status
-	fmt.Printf(
-		"Updated order %d\n status: %s",
-		order.ID, order.Status,
-	)
-	order.mu.Unlock()
-
-	updateMutex.Lock()
-	defer updateMutex.Unlock()
-	currentUpdate := totalUpdates
-	time.Sleep(5 * time.Millisecond)
-	totalUpdates = currentUpdate + 1
 }
 
 func reportOrderStatus(orders []*Order) {
